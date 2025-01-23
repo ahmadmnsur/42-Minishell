aa3bbafb1b8daf47d1c3d194ba72984aa5636ef7
Mohamad Jamil <mjamil@c1r8s2.42beirut.com>
2025-01-23 16:02:01 +0200

    first push

---
+++ minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjamil <mjamil@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 14:02:45 by mjamil            #+#    #+#             */
+/*   Updated: 2025/01/23 14:33:12 by mjamil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/stat.h>
+# include <errno.h>
+# include <signal.h>
+# include "libft/libft.h"
+
+
+#endif
