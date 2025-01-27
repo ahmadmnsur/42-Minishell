74fd26e5cfa410dec33944aac0ff2ba175b48328
Ahmad Mansour <ahmad1mansur2am@gmail.com>
2025-01-27 12:09:59 +0200

    add function add_lexer node and create new lexer node

--- src/lexer/lexer_utils.c
+++ src/lexer/lexer_utils.c
@@ -10,3 +10,39 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../minishell.h"
+
+t_lexer	*create_new_lexer_node(char *str, t_tokens token,
+        t_quote_type quote_type, int space)
+{
+    t_lexer *new_node;
+
+    new_node = (t_lexer *)calloc(1, sizeof(t_lexer));
+	if (!new_node)
+		return (NULL);
+	if (str)
+		new_node->str = ft_strdup(str);
+	else
+		new_node->str = NULL;
+	new_node->token = token;
+	new_node->quote_type = quote_type;
+	new_node->space = space;
+    new_node->i = -1;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+void	add_lexer_node_back(t_lexer **head, t_lexer *new_node)
+{
+	t_lexer *temp;
+
+	if (!*head)
+	{
+		*head = new_node;
+		return ;
+	}
+	temp = *head;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+}
