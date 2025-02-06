aa65593fd82f51b35278d34119b2f669d2f8382f
Mohamad Jamil <mohamadjamil@gmail.com>
2025-02-06 02:36:34 +0200

    add pwd function

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -10,6 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+int	pwd_help(t_env *env_list)
+{
+	int		nbr;
+	char	*pwd;
+	int		i;
+	if (!env_list)
+		return (1);
+	nbr = env_list->two_point;
+	if (nbr < 0)
+		nbr = 0;
+	pwd = get_env(env_list, "PWD");
+	if (!pwd)
+	{
+		printf("Error: PWD not found in environment variables.\n");
+		return (1);
+	}
+	printf("%s", pwd);
+	i = 0;
+	while (i < nbr)
+	{
+		printf("/..");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
 int	builtin_pwd(t_parser *parser, t_env *env)
 {
 	char	cwd[1024];
--- src/execute_command/execute1.c
+++ src/execute_command/execute1.c
@@ -27,56 +27,20 @@ int	execute_command(t_tools *tools, char **envp)
 {
 	t_parser	*parser;
 	int			nbr_of_pipe;
+    int         exit_status;
 
 	parser = tools->parser;
     nbr_of_pipe = get_pipe_len(parser);
-    nbr_of_pipe = nbr_of_pipe - 1;
-    printf("Number of pipes: %d\n", nbr_of_pipe);
-    return 0;
-}
+    if(nbr_of_pipe == 0)
+        return(0);
+    if((nbr_of_pipe - 1) == 1)
+    {
+        exit_status = if_no_pipe(tools, parser, envp);
+        set_signals();
+        return(exit_status);
+    }
+    else
+    {
 
-int main()
-{
-    t_tools tools;
-    t_parser *cmd1 = malloc(sizeof(t_parser));
-    t_parser *cmd2 = malloc(sizeof(t_parser));
-    t_parser *cmd3 = malloc(sizeof(t_parser));
-
-    if (!cmd1 || !cmd2 || !cmd3)
-        return 1;  // Exit if memory allocation fails
-
-    // Initialize the commands
-    cmd1->tokens = malloc(sizeof(t_lexer));
-    cmd2->tokens = malloc(sizeof(t_lexer));
-    cmd3->tokens = malloc(sizeof(t_lexer));
-
-    if (!cmd1->tokens || !cmd2->tokens || !cmd3->tokens)
-        return 1;  // Exit if memory allocation fails
-
-    cmd1->tokens->str = "ls";
-    cmd2->tokens->str = "grep main";
-    cmd3->tokens->str = "wc -l";
-
-    cmd1->next = cmd2;
-    cmd2->next = cmd3;
-    cmd3->next = NULL;
-
-    cmd1->prev = NULL;
-    cmd2->prev = cmd1;
-    cmd3->prev = cmd2;
-
-    tools.parser = cmd1;  // Assign the command list to tools
-
-    // Execute command
-    execute_command(&tools, NULL);
-
-    // Free allocated memory
-    free(cmd1->tokens);
-    free(cmd2->tokens);
-    free(cmd3->tokens);
-    free(cmd1);
-    free(cmd2);
-    free(cmd3);
-
-    return 0;
+    }
 }
