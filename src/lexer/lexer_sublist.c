c8aa3b64aa34a9a130ee6bc990813704b292386c
Ahmad Mansour <madrid7.am79@gmail.com>
2025-02-13 13:26:29 +0200

    edit lexer

--- src/lexer/lexer_sublist.c
+++ src/lexer/lexer_sublist.c
@@ -1,46 +1,60 @@
 #include "../../minishell.h"
 
-void	copy_lexer_node(t_lexer **sub, t_lexer **tmp)
-{
-	t_lexer	*new;
-
-	new = create_new_lexer_node((*tmp)->str, (*tmp)->token,
-			(*tmp)->quote_type, (*tmp)->space);
-	lexer_add_back(sub, new);
-	(*tmp) = (*tmp)->next;
-}
-
 void	fill_sub_lexer(t_lexer **sub, t_lexer **tmp, unsigned int sub_size)
 {
 	unsigned int	i;
+	t_lexer			*new;
 
 	i = 0;
 	while (i < sub_size)
 	{
-		copy_lexer_node(sub, tmp);
-		add_index_token(sub);
+		if ((*tmp)->token == TOKEN_EOF)
+			break;
+		new = create_new_lexer_node(
+				(*tmp)->str, (*tmp)->token, (*tmp)->quote_type, (*tmp)->space);
+		lexer_add_back(sub, new);
 		i++;
+		(*tmp) = (*tmp)->next;
+		add_index_token(sub);
 	}
 }
 
-unsigned int	get_sub_size(t_lexer *start, unsigned int nb_nodes)
+t_lexer	*find_start_node(t_lexer *lexer, t_lexer *start_node)
+{
+	t_lexer	*tmp;
+
+	tmp = lexer;
+	while (tmp && tmp->i < start_node->i)
+		tmp = tmp->next;
+	return (tmp);
+}
+
+unsigned int	count_sub_size(t_lexer *start_node, unsigned int nb_nodes)
 {
 	unsigned int	sub_size;
+	t_lexer			*tmp;
 
 	sub_size = 0;
-	while (start && sub_size < nb_nodes)
+	tmp = start_node;
+	while (tmp && sub_size < nb_nodes)
 	{
-		start = start->next;
 		sub_size++;
+		tmp = tmp->next;
 	}
 	return (sub_size);
 }
 
-t_lexer	*find_start_node(t_lexer *lexer, unsigned int start_index)
+void	fill_missing_strings(t_lexer *sub)
 {
-	while (lexer && (unsigned int) lexer->i < start_index)
-		lexer = lexer->next;
-	return (lexer);
+	t_lexer	*iter;
+
+	iter = sub;
+	while (iter)
+	{
+		if (!iter->str)
+			iter->str = get_token(iter->token);
+		iter = iter->next;
+	}
 }
 
 t_lexer	*ft_sublexer(t_lexer *lexer, t_lexer *start_node, unsigned int nb_nodes)
@@ -49,15 +63,16 @@ t_lexer	*ft_sublexer(t_lexer *lexer, t_lexer *start_node, unsigned int nb_nodes)
 	t_lexer			*tmp;
 	unsigned int	sub_size;
 
-	if (!lexer || nb_nodes == 0)
+	sub = NULL;
+	if (nb_nodes == 0 || !lexer)
 		return (NULL);
-	tmp = find_start_node(lexer, start_node->i);
+	tmp = find_start_node(lexer, start_node);
 	if (!tmp)
 		return (NULL);
-	sub_size = get_sub_size(tmp, nb_nodes);
+	sub_size = count_sub_size(tmp, nb_nodes);
 	if (sub_size == 0)
 		return (NULL);
-	sub = NULL;
 	fill_sub_lexer(&sub, &tmp, sub_size);
+	fill_missing_strings(sub);
 	return (sub);
 }
