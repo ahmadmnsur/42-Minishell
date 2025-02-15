69a2f8c45f376c37c7e26e8c42c973b472390d1a
Mohamad Jamil <mohamadjamil@gmail.com>
2025-02-15 14:35:08 +0200

    add art

--- minishell.c
+++ minishell.c
@@ -20,6 +20,7 @@ int	main(int argc, char *argv[], char *envp[])
 	char	*cmd;
 	t_tools	tools;
 
+	art();
 	set_init(argc, argv, &(tools.last_exit_status), &(tools.env));
 	(copy_envp(&(tools.env), envp), initialize_environment(&(tools.env)));
 	while (1)
