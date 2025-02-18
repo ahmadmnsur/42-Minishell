737c14a17f610038036f6fc4540bd4e99027cbaa
Ahmad Mansour <madrid7.am79@gmail.com>
2025-02-18 07:30:26 +0200

    add if condition to assign token to parser function

--- src/parser/tokens.c
+++ src/parser/tokens.c
@@ -39,6 +39,8 @@ static void	assign_tokens_to_parser(t_parser **parser,
 	start_node = tmp_lexer;
 	while (tmp_parser && tmp_lexer)
 	{
+		if (start_node && start_node->token == TOKEN_EOF)
+			break;
 		while (tmp_lexer && tmp_lexer->token != TOKEN_PIPE && tmp_lexer->token != TOKEN_SEMICOLON)
 			tmp_lexer = tmp_lexer->next;
 		if (tmp_lexer)
