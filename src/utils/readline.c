abd43f534644c8cf5936211fc267e6bdb53b33a5
Mohamad Jamil <mohamadjamil@gmail.com>
2025-02-25 17:27:00 +0200

    new readline

--- src/utils/readline.c
+++ src/utils/readline.c
@@ -12,28 +12,34 @@
 
 #include "../../minishell.h"
 
+// char	*minishell_loop(int status)
+// {
+// 	char	*prompt;
+// 	char	*last_status;
+// 	char	*readline_;
+
+// 	if (status == 0)
+// 	{
+// 		prompt = ft_strdup("\001\033[32m\002minishell\001\033[0m\002\001\033[34m\002[0]\001\033[0m\002$ ");
+// 		readline_ = readline(prompt);
+// 		free(prompt);
+// 	}
+// 	else
+// 	{
+// 		prompt = ft_strdup("\001\033[32m\002minishell\001\033[0m\002");
+// 		prompt = ft_strjoin(prompt, "\001\033[31m\002[");
+// 		last_status = ft_itoa(status);
+// 		prompt = ft_strjoin(prompt, last_status);
+// 		free(last_status);
+// 		prompt = ft_strjoin(prompt, "]\001\033[0m\002$ ");
+// 		readline_ = readline(prompt);
+// 		free(prompt);
+// 	}
+// 	return (readline_);
+// }
+
 char	*minishell_loop(int status)
 {
-	char	*prompt;
-	char	*last_status;
-	char	*readline_;
-
-	if (status == 0)
-	{
-		prompt = ft_strdup("\001\033[32m\002minishell\001\033[0m\002\001\033[34m\002[0]\001\033[0m\002$ ");
-		readline_ = readline(prompt);
-		free(prompt);
-	}
-	else
-	{
-		prompt = ft_strdup("\001\033[32m\002minishell\001\033[0m\002");
-		prompt = ft_strjoin(prompt, "\001\033[31m\002[");
-		last_status = ft_itoa(status);
-		prompt = ft_strjoin(prompt, last_status);
-		free(last_status);
-		prompt = ft_strjoin(prompt, "]\001\033[0m\002$ ");
-		readline_ = readline(prompt);
-		free(prompt);
-	}
-	return (readline_);
+	(void)status;
+	return (readline("minishell>>"));
 }
